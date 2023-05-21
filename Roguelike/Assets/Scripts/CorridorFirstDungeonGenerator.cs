1e538cb479a5e68e8d2028eceeb31813b6fa250c
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2023-05-21 18:23:59 +0300

    +Fixing Dead Ends

--- Roguelike/Assets/Scripts/CorridorFirstDungeonGenerator.cs
+++ Roguelike/Assets/Scripts/CorridorFirstDungeonGenerator.cs
@@ -22,10 +22,39 @@ public class CorridorFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator {
 
 		HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPositions);
 
-		floorPositions.UnionWith(roomPositions); ;
+		List<Vector2Int> deadEnds = FindAllDeadEnds(floorPositions);
+
+		CreateRoomsAtDeadEnd(deadEnds, roomPositions);
+
+		floorPositions.UnionWith(roomPositions);
 
 		TilemapVisualizer.PaintFloorTiles(floorPositions);
 		WallGenerator.CreateWalls(floorPositions, TilemapVisualizer);
+	}
+
+	private void CreateRoomsAtDeadEnd(List<Vector2Int> deadEnds, HashSet<Vector2Int> roomFloors) {
+		foreach (var position in deadEnds) {
+			if (roomFloors.Contains(position) == false) {
+				var room = RunRandomWalk(randomWalkParameters, position);
+				roomFloors.UnionWith(room);
+			}
+		}
+	}
+
+	private List<Vector2Int> FindAllDeadEnds(HashSet<Vector2Int> floorPositions) {
+		List<Vector2Int> deadEnds = new List<Vector2Int>();
+		foreach (var position in floorPositions) {
+			int neighboursCount = 0;
+			foreach (var direction in Direction2D.cardinalDirectionsList) {
+				if (floorPositions.Contains(position + direction)) {
+					neighboursCount++;
+				}
+			}
+			if (neighboursCount == 1) {
+				deadEnds.Add(position);
+			}
+		}
+		return deadEnds;
 	}
 
 	private HashSet<Vector2Int> CreateRooms(HashSet<Vector2Int> potentialRoomPositions) {
